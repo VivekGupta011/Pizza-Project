3e51f0f87be895a80ffd8b194ea33d914641693c
Vivek_Gupta <vivekgupta86988@gmail.com>
2022-10-25 08:48:59 +0530

    order page

--- project/src/Order.js
+++ project/src/Order.js
@@ -13,16 +13,34 @@ import {OrderDelivery} from "./OrderDelivery";
 export function Order() {
   const navigate=useNavigate();
   const [isAdding, setIsAdding] = useState(false);
+  const [isAdding1, setIsAdding1] = useState(false);
+  const [isAdding2, setIsAdding2] = useState(false);
   const [disabled, setdisabled] = useState(false);
+  const [disabled1, setdisabled1] = useState(false);
+  const [disabled2, setdisabled2] = useState(false);
   const [isSubtract, setIsSubtract] = useState(false);
+  const [isSubtract1, setIsSubtract1] = useState(false);
+  const [isSubtract2, setIsSubtract2] = useState(false);
   const [counter, setCounter] = useState(1);
   const [counter1, setCounter1] = useState(1);
   const [counter2, setCounter2] = useState(1);
   const incrementCounter = () => {
     setCounter(counter + 1);
   };
+  const incrementCounter1 = () => {
+    setCounter1(counter1 + 1);
+  };
+  const incrementCounter2 = () => {
+    setCounter2(counter2 + 1);
+  };
   const decrementCounter = () => {
     setCounter(counter - 1);
+  };
+  const decrementCounter1 = () => {
+    setCounter1(counter1 - 1);
+  };
+  const decrementCounter2 = () => {
+    setCounter2(counter2 - 1);
   };
   if (counter <= 0) {
     incrementCounter = () => {
@@ -34,6 +52,14 @@ export function Order() {
     incrementCounter();
     setIsAdding(true);
   }
+  function AddElement1() {
+    incrementCounter1();
+    setIsAdding1(true);
+  }
+  function AddElement2() {
+    incrementCounter2();
+    setIsAdding2(true);
+  }
   function TestingNumber() {
     if (counter <= 0) {
       alert("atleast one item select..");
@@ -44,6 +70,16 @@ export function Order() {
     decrementCounter();
     setIsSubtract(true);
   }
+  function deleteElement1() {
+    TestingNumber();
+    decrementCounter1();
+    setIsSubtract1(true);
+  }
+  function deleteElement2() {
+    TestingNumber();
+    decrementCounter2();
+    setIsSubtract2(true);
+  }
 
   function orderFunction(){
     alert("Thank you,your order has been successfully completed!");
@@ -52,8 +88,12 @@ export function Order() {
 
   setTimeout(() => {
     setIsAdding(false);
+    setIsAdding1(false);
+    setIsAdding2(false);
     setdisabled(false);
     setIsSubtract(false);
+    setIsSubtract1(false);
+    setIsSubtract2(false);
     
   }, 2000);
   return (
@@ -131,24 +171,24 @@ export function Order() {
             </div>
             <div className="Pricing d-flex justify-content-center IncrementClass">
               <button
-                disabled={isAdding}
+                disabled={isAdding1}
                 onClick={() => {
-                  AddElement();
+                  AddElement1();
                 }}
                 className={"btn btn-secondary custom-button"}
               >
-                +{isAdding ? "Added" : ""}
+                +{isAdding1 ? "Added" : ""}
               </button>
 
-              <strong>{counter}</strong>
+              <strong>{counter1}</strong>
               <button
                 disabled={disabled}
                 onClick={() => {
-                  counter1 == 1 ? setdisabled(true) : deleteElement();
+                  counter1 == 1 ? setdisabled(true) : deleteElement1();
                 }}
                 className={"btn btn-secondary custom-button"}
               >
-                -{isSubtract ? "Subract" : ""}
+                -{isSubtract1 ? "Subract" : ""}
               </button>
             </div>
           </div>
@@ -165,24 +205,24 @@ export function Order() {
             </div>
             <div className="Pricing d-flex justify-content-center IncrementClass">
               <button
-                disabled={isAdding}
+                disabled={isAdding2}
                 onClick={() => {
-                  AddElement();
+                  AddElement2();
                 }}
                 className={"btn btn-secondary custom-button"}
               >
-                +{isAdding ? "Added" : ""}
+                +{isAdding2 ? "Added" : ""}
               </button>
 
-              <strong>{counter}</strong>
+              <strong>{counter2}</strong>
               <button
                 disabled={disabled}
                 onClick={() => {
-                  counter2 == 1 ? setdisabled(true) : deleteElement();
+                  counter2 == 1 ? setdisabled(true) : deleteElement2();
                 }}
                 className={"btn btn-secondary custom-button"}
               >
-                -{isSubtract ? "Subract" : ""}
+                -{isSubtract2 ? "Subract" : ""}
               </button>
             </div>
           </div>
